8eeb3b7340cc1379812870ab1a5d17cafe56ba5f
xuealex2010-blip <xuealex2010@gmail.com>
2026-09-04 19:41:48 -0400

    Add files via upload

---
+++ button.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("button")
+const counter = document.getElementById("counter")
+
+button.addEventListener("click", () => counter.innerHTML++)
+
+//function clickHandler() {
+//    counter.InnterHTML++
+//}
